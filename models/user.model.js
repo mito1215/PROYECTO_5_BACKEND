46a0b72151b5e719cb47854c0f3c46953be89838
Miguel <miguelcorrea15@hotmail.com>
2023-09-26 18:18:15 -0500

    first commit

---
+++ models/user.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+
+//Crear SCHEMA o esquema de la tabla usuario
+const userSchema = new mongoose.Schema({
+    DNI: { type: Number, required: true},
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    birthdate: { type: Date, required: true },
+    role: { type: String, required: true },
+    cellPhoone: { type: Number, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    avatar: {type: String, required: false},
+    userName: { type: String, required: true }
+});
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject._v;
+    }
+})
+
+//Encriptar password
+userSchema.methods.encryptPassword = async (password) => {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+
+    return hash;
+}
+
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+}
+
+//USER MODEL o Crear la coleccion
+export const User = mongoose.model(
+    'User', 
+    userSchema
+);/*El nombre debe ser siempre en singular User*/
