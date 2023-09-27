8ce79ac39881ab7c04b671a0edf20e07b77b083d
Miguel <miguelcorrea15@hotmail.com>
2023-09-27 09:07:56 -0500

    cambio de servidor en los comentarios

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,29 +8,29 @@ export const userRouter = Router();
 //ROUTES
 //CRUD
 //GET all users
-//htpp://localhost:3000/api/v1/users
+//htpp://localhost:3002/rentcar/api/v1/users
 userRouter.get("/", getUser);
 
 //GET User by id
-//htpp://localhost:3000/api/v1/users/:id
+//htpp://localhost:3002/rentcar/api/v1/users/:id
 userRouter.get("/:id", getUserId );
 
-//POST htpp://localhost:3000/api/v1/users
+//POST htpp://localhost:3002/rentcar/api/v1/users
 userRouter.post("/", simpleMiddleware, postUser);
 
 //Loguearse
-//POST htpp://localhost:3000/api/v1/users/login
+//POST htpp://localhost:3002/rentcar/api/v1/users/login
 userRouter.post("/login", login);
 
 //Subir avatar
-//POST htpp://localhost:3000/api/v1/users/avatar/id
+//POST htpp://localhost:3002/rentcar/api/v1/users/avatar/id
 userRouter.post("/avatar/:id", upload.single('avatar') , uploadAvatar);
 
 //UPDATE User
-//Put or Patch htpp://localhost:3000/api/v1/users/:id
+//Put or Patch htpp://localhost:3002/rentcar/api/v1/users/:id
 //Put reemplaza todo el recurso y Patch actualiza el recurso
 userRouter.patch("/:id", simpleMiddleware, updateUser);
 
 //DELETE User by id
-//Delete htpp://localhost:3000/api/v1/users/:id
+//Delete htpp://localhost:3002/rentcar/api/v1/users/:id
 userRouter.delete("/:id", deleteUser);
