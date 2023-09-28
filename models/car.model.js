2cfa11b3afccdbe8a125da3159992babc99f39e2
Miguel <miguelcorrea15@hotmail.com>
2023-09-27 21:11:38 -0500

    car table creation

---
+++ models/car.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+//Crear SCHEMA o esquema de la tabla Car
+const carSchema = new mongoose.Schema({
+    carRegister: {type: Number, require: true},
+    carBrand: {type: String, require: true},
+    carLine: {type: String, require: true},
+    carModel: {type: Number, require: true},
+    carColor: {type: String, require: true},
+    carFuel: {type: String, require: true},
+});
+
+export const Car = mongoose.model(
+    "Car", 
+    carSchema
+);
